ac2a12fd503487a0cbbfb6de6f1ba08367f56725
Sylwow <Sylwo1003@gmail.com>
2022-03-23 17:57:48 +0100

    upgrade date class

--- Source/Library/Time.cpp
+++ Source/Library/Time.cpp
@@ -3,18 +3,36 @@
 #include <format>
 #include <tuple>
 
-
 namespace sd
 {
+    using Year = std::chrono::year;
+    using Month = std::chrono::month;
+    using Day = std::chrono::day;
+    using Years = std::chrono::years;
+    using Months = std::chrono::months;
+    using Days = std::chrono::days;
+    using Weaks = std::chrono::weeks;
+    using WeakDay = std::chrono::weekday;
+    using Hours = std::chrono::hours;
+    using Minutes = std::chrono::minutes;
+    using Seconds = std::chrono::seconds;
+    using Milliseconds = std::chrono::milliseconds;
+    using YearMontDay = std::chrono::year_month_day;
+    using HHMMSS = std::chrono::hh_mm_ss<Milliseconds>;
 
     using namespace std::chrono;
     namespace
     {
-        template <class T, class D> Time from(D duration)
-        { // 86400000000 microseconds
+        template <class T> T roundToZero(Microseconds timePoint)
+        {
+            bool isLessThanZero = timePoint < Microseconds::zero();
+            return isLessThanZero ? ceil<T>(timePoint) : floor<T>(timePoint);
+        }
+
+        template <class T, class D> long long from(D duration)
+        {
             Microseconds micro = duration_cast<Microseconds>(T{1});
-            auto newValue = static_cast<long long>(duration * micro.count());
-            return Time::fromMicroseconds(newValue);
+            return static_cast<long long>(duration * micro.count());
         }
 
         template <class T> double round(Microseconds dur)
@@ -25,7 +43,7 @@ namespace sd
 
         auto decomposeDays(Microseconds timePoint)
         {
-            auto days = floor<Days>(timePoint);
+            auto days = roundToZero<Days>(timePoint);
             auto rest = timePoint - days;
             return std::make_pair(days, rest);
         }
@@ -33,7 +51,7 @@ namespace sd
         auto decomposeHHMMSS(Microseconds timePoint)
         {
             auto [days, rest] = decomposeDays(timePoint);
-            auto restInMili = floor<Milliseconds>(rest);
+            auto restInMili = roundToZero<Milliseconds>(rest);
             auto restMicroseconds = rest - restInMili;
             HHMMSS hh{restInMili};
             return std::make_pair(hh, restMicroseconds);
@@ -44,25 +62,35 @@ namespace sd
     Time Time::min() { return Time{Microseconds::min().count()}; }
     Time Time::zero() { return Time{Microseconds::zero().count()}; }
 
-    Time Time::parse(std::string source, std::string format) {
+    Time Time::parse(std::string source, std::string format)
+    {
         Time time{0};
         std::stringstream st{source};
         std::chrono::from_stream(st, format.c_str(), time._time);
         return time;
     }
 
+    namespace dev
+    {
+        namespace ch = std::chrono;
 
-    Time Time::fromDays(double days) { return from<Days>(days); }
-    Time Time::fromHours(double hours) { return from<Hours>(hours); }
-    Time Time::fromMinutes(double minutes) { return from<Minutes>(minutes); }
-    Time Time::fromSeconds(double seconds) { return from<Seconds>(seconds); }
-    Time Time::fromMiliseconds(double miliseconds) { return from<Milliseconds>(miliseconds); }
-    Time Time::fromDays(int days) { return from<Days>(days); }
-    Time Time::fromHours(int hours) { return from<Hours>(hours); }
-    Time Time::fromMinutes(int minutes) { return from<Minutes>(minutes); }
-    Time Time::fromSeconds(int seconds) { return from<Seconds>(seconds); }
-    Time Time::fromMiliseconds(int miliseconds) { return from<Milliseconds>(miliseconds); }
-    Time Time::fromMicroseconds(long long microseconds) { return Time{microseconds}; }
+        Days::Days(int days) : Time{from<ch::days>(days)} {}
+        Days::Days(double days) : Time{from<ch::days>(days)} {}
+
+        Hours::Hours(int hours) : Time{from<ch::hours>(hours)} {}
+        Hours::Hours(double hours) : Time{from<ch::hours>(hours)} {}
+
+        Minutes::Minutes(int minutes) : Time{from<ch::minutes>(minutes)} {}
+        Minutes::Minutes(double minutes) : Time{from<ch::minutes>(minutes)} {}
+
+        Seconds::Seconds(int seconds) : Time{from<ch::seconds>(seconds)} {}
+        Seconds::Seconds(double seconds) : Time{from<ch::seconds>(seconds)} {}
+
+        Miliseconds::Miliseconds(int miliseconds) : Time{from<ch::milliseconds>(miliseconds)} {}
+        Miliseconds::Miliseconds(double miliseconds) : Time{from<ch::milliseconds>(miliseconds)} {}
+
+        Microseconds::Microseconds(long long microseconds) : Time{microseconds} {}
+    } // namespace dev
 
     Time::Time(long long microseconds) : _time{Microseconds{microseconds}} {}
     Time::Time(int hour, int minute, int second) : _time{Hours{hour} + Minutes{minute} + Seconds{second}} {}
@@ -88,10 +116,7 @@ namespace sd
 
     Time Time::duration() const { return Time{abs(_time).count()}; }
 
-    std::string Time::toString(std::string format) const
-    {
-        return std::format(format, _time);
-    }
+    std::string Time::toString(std::string format) const { return std::format(format, _time); }
 
     Time &Time::add(const Time &other)
     {
@@ -124,10 +149,10 @@ namespace sd
     Time &Time::operator*=(double factor) { return multiply(factor); }
     Time &Time::operator/=(double factor) { return divide(factor); }
 
-    Time Time::operator+(const Time &other) { return Time{*this}.add(other); }
-    Time Time::operator-(const Time &other) { return Time{*this}.substract(other); }
-    Time Time::operator*(double factor) { return Time{*this}.multiply(factor); }
-    Time Time::operator/(double factor) { return Time{*this}.divide(factor); }
+    Time Time::operator+(const Time &other) const { return Time{*this}.add(other); }
+    Time Time::operator-(const Time &other) const { return Time{*this}.substract(other); }
+    Time Time::operator*(double factor) const { return Time{*this}.multiply(factor); }
+    Time Time::operator/(double factor) const { return Time{*this}.divide(factor); }
 
     bool operator==(const Time &lhs, const Time &rhs) { return lhs.totalMicroseconds() == rhs.totalMicroseconds(); }
     bool operator!=(const Time &lhs, const Time &rhs) { return lhs.totalMicroseconds() != rhs.totalMicroseconds(); }
@@ -136,26 +161,32 @@ namespace sd
     bool operator>(const Time &lhs, const Time &rhs) { return lhs.totalMicroseconds() > rhs.totalMicroseconds(); }
     bool operator>=(const Time &lhs, const Time &rhs) { return lhs.totalMicroseconds() >= rhs.totalMicroseconds(); }
 
-    Time Time::operator+() { return Time{*this}; }
-    Time Time::operator-() { return Time{*this}.negate(); }
+    Time Time::operator+() const { return Time{*this}; }
+    Time Time::operator-() const { return Time{*this}.negate(); }
 
-    Time operator""_d(long double days) { return Time::fromDays(static_cast<double>(days)); }
-    Time operator""_d(unsigned long long days) { return Time::fromDays(static_cast<int>(days)); }
+    dev::Days operator""_ds(long double days) { return dev::Days(static_cast<double>(days)); }
+    dev::Days operator""_ds(unsigned long long days) { return dev::Days(static_cast<int>(days)); }
 
-    Time operator""_h(long double hours) { return Time::fromHours(static_cast<double>(hours)); }
-    Time operator""_h(unsigned long long hours) { return Time::fromHours(static_cast<int>(hours)); }
+    dev::Hours operator""_h(long double hours) { return dev::Hours(static_cast<double>(hours)); }
+    dev::Hours operator""_h(unsigned long long hours) { return dev::Hours(static_cast<int>(hours)); }
 
-    Time operator""_min(long double minutes) { return Time::fromMinutes(static_cast<double>(minutes)); }
-    Time operator""_min(unsigned long long minutes) { return Time::fromMinutes(static_cast<int>(minutes)); }
+    dev::Minutes operator""_min(long double minutes) { return dev::Minutes(static_cast<double>(minutes)); }
+    dev::Minutes operator""_min(unsigned long long minutes) { return dev::Minutes(static_cast<int>(minutes)); }
 
-    Time operator""_s(long double seconds) { return Time::fromSeconds(static_cast<double>(seconds)); }
-    Time operator""_s(unsigned long long seconds) { return Time::fromSeconds(static_cast<int>(seconds)); }
+    dev::Seconds operator""_s(long double seconds) { return dev::Seconds(static_cast<double>(seconds)); }
+    dev::Seconds operator""_s(unsigned long long seconds) { return dev::Seconds(static_cast<int>(seconds)); }
 
-    Time operator""_ms(long double miliseconds) { return Time::fromMiliseconds(static_cast<double>(miliseconds)); }
-    Time operator""_ms(unsigned long long miliseconds) { return Time::fromMiliseconds(static_cast<int>(miliseconds)); }
-
-    Time operator""_us(unsigned long long microseconds)
+    dev::Miliseconds operator""_ms(long double miliseconds)
     {
-        return Time::fromMicroseconds(static_cast<long long>(microseconds));
+        return dev::Miliseconds(static_cast<double>(miliseconds));
+    }
+    dev::Miliseconds operator""_ms(unsigned long long miliseconds)
+    {
+        return dev::Miliseconds(static_cast<int>(miliseconds));
+    }
+
+    dev::Microseconds operator""_us(unsigned long long microseconds)
+    {
+        return dev::Microseconds(static_cast<long long>(microseconds));
     }
 } // namespace sd
--- Source/Library/h/Time.hpp
+++ Source/Library/h/Time.hpp
@@ -5,117 +5,135 @@
 
 namespace sd
 {
-using Year = std::chrono::year;
-using Month = std::chrono::month;
-using Day = std::chrono::day;
-using Years = std::chrono::years;
-using Months = std::chrono::months;
-using Days = std::chrono::days;
-using Weaks = std::chrono::weeks;
-using WeakDay = std::chrono::weekday;
-using Hours = std::chrono::hours;
-using Minutes = std::chrono::minutes;
-using Seconds = std::chrono::seconds;
-using Milliseconds = std::chrono::milliseconds;
-using Microseconds = std::chrono::microseconds;
-using YearMontDay = std::chrono::year_month_day;
-using HHMMSS = std::chrono::hh_mm_ss<Milliseconds>;
 
-template <class Rep, class Period = std::ratio<1> >
-using Duration = std::chrono::duration<Rep, Period>;
+    using Microseconds = std::chrono::microseconds;
 
-class Time
-{
-private:
-  Microseconds _time;
+    template <class Rep, class Period = std::ratio<1>> using Duration = std::chrono::duration<Rep, Period>;
 
-public:
-  static Time max ();
-  static Time min ();
-  static Time zero ();
+    struct Time
+    {
+      private:
+        Microseconds _time;
 
-  static Time parse (std::string source, std::string format = "%T");
+      public:
+        static Time max();
+        static Time min();
+        static Time zero();
 
-  static Time fromDays (double days);
-  static Time fromHours (double hours);
-  static Time fromMinutes (double minutes);
-  static Time fromSeconds (double seconds);
-  static Time fromMiliseconds (double miliseconds);
-  static Time fromDays (int days);
-  static Time fromHours (int hours);
-  static Time fromMinutes (int minutes);
-  static Time fromSeconds (int seconds);
-  static Time fromMiliseconds (int miliseconds);
-  static Time fromMicroseconds (long long microseconds);
+        static Time parse(std::string source, std::string format = "%T");
 
-  Time (long long microseconds);
-  Time (int hour, int minute, int second);
-  Time (int days, int hour, int minute, int second, int miliseconds = 0,
-        int microseconds = 0);
+        Time(long long microseconds);
+        Time(int hour, int minute, int second);
+        Time(int days, int hour, int minute, int second, int miliseconds = 0, int microseconds = 0);
 
-  Time (const Time &other) = default;
+        Time(const Time &other) = default;
 
-  int days () const;
-  int hours () const;
-  int minutes () const;
-  int seconds () const;
-  int miliseconds () const;
-  int microseconds () const;
+        int days() const;
+        int hours() const;
+        int minutes() const;
+        int seconds() const;
+        int miliseconds() const;
+        int microseconds() const;
 
-  double totalDays () const;
-  double totalHours () const;
-  double totalMinutes () const;
-  double totalSeconds () const;
-  double totalMiliseconds () const;
-  long long totalMicroseconds () const;
+        double totalDays() const;
+        double totalHours() const;
+        double totalMinutes() const;
+        double totalSeconds() const;
+        double totalMiliseconds() const;
+        long long totalMicroseconds() const;
 
-  Time duration () const;
+        Time duration() const;
 
-  std::string toString (std::string format = "{:%T}") const;
+        std::string toString(std::string format = "{:%T}") const;
 
-  Time &add (const Time &other);
-  Time &substract (const Time &other);
-  Time &multiply (double factor);
-  Time &divide (double factor);
-  Time &negate ();
+        Time &add(const Time &other);
+        Time &substract(const Time &other);
+        Time &multiply(double factor);
+        Time &divide(double factor);
+        Time &negate();
 
-  Time &operator= (const Time &other) = default;
+        Time &operator=(const Time &other) = default;
 
-  Time &operator+= (const Time &other);
-  Time &operator-= (const Time &other);
-  Time &operator*= (double factor);
-  Time &operator/= (double factor);
+        Time &operator+=(const Time &other);
+        Time &operator-=(const Time &other);
+        Time &operator*=(double factor);
+        Time &operator/=(double factor);
 
-  Time operator+ (const Time &other);
-  Time operator- (const Time &other);
-  Time operator* (double factor);
-  Time operator/ (double factor);
+        Time operator+(const Time &other) const;
+        Time operator-(const Time &other) const;
+        Time operator*(double factor) const;
+        Time operator/(double factor) const;
 
-  Time operator+ ();
-  Time operator- ();
-};
+        Time operator+() const;
+        Time operator-() const;
+    };
 
-bool operator== (const Time &lhs, const Time &rhs);
-bool operator!= (const Time &lhs, const Time &rhs);
-bool operator< (const Time &lhs, const Time &rhs);
-bool operator<= (const Time &lhs, const Time &rhs);
-bool operator> (const Time &lhs, const Time &rhs);
-bool operator>= (const Time &lhs, const Time &rhs);
+    bool operator==(const Time &lhs, const Time &rhs);
+    bool operator!=(const Time &lhs, const Time &rhs);
+    bool operator<(const Time &lhs, const Time &rhs);
+    bool operator<=(const Time &lhs, const Time &rhs);
+    bool operator>(const Time &lhs, const Time &rhs);
+    bool operator>=(const Time &lhs, const Time &rhs);
 
-Time operator"" _d (long double days);
-Time operator"" _d (unsigned long long days);
+    namespace dev
+    {
+        struct Days : public Time
+        {
+            Days(int days);
+            Days(double days);
+            Days(const Days &) = default;
+            Days &operator=(const Days &other) = default;
+        };
+        struct Hours : public Time
+        {
+            Hours(int hours);
+            Hours(double hours);
+            Hours(const Hours &) = default;
+            Hours &operator=(const Hours &other) = default;
+        };
+        struct Minutes : public Time
+        {
+            Minutes(int minutes);
+            Minutes(double minutes);
+            Minutes(const Minutes &) = default;
+            Minutes &operator=(const Minutes &other) = default;
+        };
+        struct Seconds : public Time
+        {
+            Seconds(int seconds);
+            Seconds(double seconds);
+            Seconds(const Seconds &) = default;
+            Seconds &operator=(const Seconds &other) = default;
+        };
+        struct Miliseconds : public Time
+        {
+            Miliseconds(int miliseconds);
+            Miliseconds(double miliseconds);
+            Miliseconds(const Miliseconds &) = default;
+            Miliseconds &operator=(const Miliseconds &other) = default;
+        };
+        struct Microseconds : public Time
+        {
+            Microseconds(long long microseconds);
+            Microseconds(const Microseconds &) = default;
+            Microseconds &operator=(const Microseconds &other) = default;
+        };
+    } // namespace dev
 
-Time operator"" _h (long double hours);
-Time operator"" _h (unsigned long long hours);
+    dev::Days operator"" _ds(long double days);
+    dev::Days operator"" _ds(unsigned long long days);
 
-Time operator"" _min (long double minutes);
-Time operator"" _min (unsigned long long minutes);
+    dev::Hours operator"" _h(long double hours);
+    dev::Hours operator"" _h(unsigned long long hours);
 
-Time operator"" _s (long double seconds);
-Time operator"" _s (unsigned long long seconds);
+    dev::Minutes operator"" _min(long double minutes);
+    dev::Minutes operator"" _min(unsigned long long minutes);
 
-Time operator"" _ms (long double miliseconds);
-Time operator"" _ms (unsigned long long miliseconds);
+    dev::Seconds operator"" _s(long double seconds);
+    dev::Seconds operator"" _s(unsigned long long seconds);
 
-Time operator"" _us (unsigned long long microseconds);
+    dev::Miliseconds operator"" _ms(long double miliseconds);
+    dev::Miliseconds operator"" _ms(unsigned long long miliseconds);
+
+    dev::Microseconds operator"" _us(unsigned long long microseconds);
 } // namespace sd
